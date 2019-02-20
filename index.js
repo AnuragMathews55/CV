b2c1f8bd76fbbce5cbefbe88b157d233dc407c27
AnuragMathews55 <46888828+AnuragMathews55@users.noreply.github.com>
2019-02-20 21:36:36 +0530

    CV Updated V1

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function myfunction() {
+  location.replace("skills.html");
+}
+function myfunction1() {
+  location.replace("workexp.html");
+}
+function myfunction2() {
+  location.replace("project.html");
+}
+function myfunction3() {
+  location.replace("underconstruct.html");
+}
+function myfunction4() {
+  location.replace("underconstruct.html");
+}
+function myfunction5() {
+  location.replace("underconstruct.html");
+}
